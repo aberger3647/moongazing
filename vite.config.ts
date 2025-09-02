dee7c5ba67ba5bce66becdaec0cd2b99a65cedac
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2025-09-01 19:06:19 -0500

    define __DEFINES__ as empty obj

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __DEFINES__: {}, // <-- just define it as an empty object
+  },
 })
